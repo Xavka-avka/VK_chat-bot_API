51ae59eca58578058467f060850485b7161b871d
Vyacheslav <slavatrifonov22667@gmail.com>
2023-03-10 21:20:21 +0400

    release v1.0

---
+++ adds.py
@@ -0,0 +1,28 @@
+token_vk_bot = 'vk1.a.Oyti_nnCUq_IFPZyEk_imJcvqIefrsrKIn5tdIe2rpUQrOZ9Tnw6_cdomgc4lKlr' \
+               '6AyOFe9EpZ-DRWAZssFyWhQ7BMXrjIVQRorDgGpa8C3SP_bYBad9wIKtCbOr5C9aUMtuc4' \
+               '5b1mTYcfoEuU3gvnym2RhNIgXkL93nQqSXrs_6wb-CqNnMsqL3-nd9m71lcYYEwG6Lcbf44OLjdRYmMw'
+
+group_id = '218034602'
+
+y_api_headers = {'X-Yandex-API-Key': '950cc440-5fd2-452d-a099-ca9d166c2241'}
+
+g_api_params = {'apiKey': 'c63f4053f7924ace8a63e2f6e6c7a12d'}
+
+cr_token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.e' \
+           'yJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjgyOTI3Y2QxLTkzMTQtNDNhYy1hYjM2LTNmZT' \
+           'hiZGE4ZWFmOCIsImlhdCI6MTY3ODQ1NjgwMCwic3ViIjoiZGV2ZWxvcGVyL2YzNDllOGNjLTYzNTEtZDAxZC0wOGZhLWM0MzllMTgxZWI' \
+           '2NCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5n' \
+           'In0seyJjaWRycyI6WyI0Ni4wLjMxLjc1Il0sInR5cGUiOiJjbGllbnQifV19.9EJGUkmOPvOzeFQ8PSj9aXq2v1yRKH-aokoJGIY0GPYv' \
+           'yDcJgGQP4UWEqfwivPSqMq0pldnXwAlaOa8UmIDBIw'
+
+cr_link = 'https://api.clashroyale.com/v1'
+
+cr_header = {'authorization': cr_token}
+
+f_help = f'{"❖"*30}\nВсе доступные команды чат-бота в данный момент:\n' \
+         f'\n/Привет: возвращает "Привет!"\n' \
+         f'\n/Пока: возвращает "Пока!"\n' \
+         f'\n/Кубик: кидает кубик для игры кости и возвращает его картинку\n' \
+         f'\n/Погода: возвращает погоду по IP адресу моего компьютера\n' \
+         f'\n/Сундуки (тег игрока Clash Royale без #): ' \
+         f'возвращает названия 8 сундуков, которые выпадут этому игроку следующими\n{"❖"*30}'
---
+++ utils.py
@@ -0,0 +1,45 @@
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+
+def centered_text(text, img, pos, rgb):
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype('cr/Supercell-Magic_1.ttf', 24)
+    _, _, size_w, size_h = draw.textbbox((0, 0), text, font=font)
+    draw.text((pos[0] - size_w // 2, pos[1] - size_h // 2),
+              text, (rgb[0], rgb[1], rgb[2]), font=font)
+    return size_w, size_h
+
+
+def add_text(text, img, pos, rgb):
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype('cr/Supercell-Magic_1.ttf', 24)
+    _, _, size_w, size_h = draw.textbbox((0, 0), text, font=font)
+    draw.text(pos, text, rgb, font=font)
+    return size_w, size_h
+
+
+def add_chest(chest, x, y, background, number):
+    background.paste(chest, box=(x, y), mask=chest)
+    add_text(str(number), background, (x + 40, y + 100), (255, 255, 255))
+
+
+chests = {}
+
+
+def get_chests():
+    if not chests:
+        for i in range(len(chest_names)):
+            _chest = Image.open(f'cr/{list(chest_names)[i]}.png', 'r')
+            _chest.thumbnail((100, 100))
+            chests[list(chest_names)[i]] = _chest
+    return chests
+        
+
+chest_names = {
+    'Wooden chest': 'wooden_chest', 'Silver Chest': 'silver_chest', 'Golden Chest': 'golden_chest',
+    'Magical Chest': 'magical_chest', 'Giant Chest': 'giant_chest', 'Mega Lightning Chest': 'mega_lightning_chest',
+    'Epic Chest': 'epic_chest', 'Legendary chest': 'legendary_chest', 'Gold Crate': 'gold_crate',
+    'Plentiful Gold Crate': 'plentiful_gold_crate', 'Overflowing Gold Crate': 'overflowing_gold_crate',
+    'Royal Wild Chest': 'royal_wild_chest'}
