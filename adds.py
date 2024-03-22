a464796bb4f6439e26555e21cd2ababa942b1974
Vyacheslav <slavatrifonov22667@gmail.com>
2024-03-22 19:28:21 +0400

    a little bit cringe for 23 March 2024

--- adds.py
+++ adds.py
@@ -8,12 +8,7 @@ y_api_headers = {'X-Yandex-API-Key': '950cc440-5fd2-452d-a099-ca9d166c2241'}
 
 g_api_params = {'apiKey': 'c63f4053f7924ace8a63e2f6e6c7a12d'}
 
-cr_token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.e' \
-           'yJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImIxMWRjYTViLTc3NGUtNDM3ZS05ODBkLTFmNj' \
-           'E0ZWEyODIwNSIsImlhdCI6MTY3ODgwNDI3Niwic3ViIjoiZGV2ZWxvcGVyL2YzNDllOGNjLTYzNTEtZDAxZC0wOGZhLWM0MzllMTgxZWI' \
-           '2NCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5n' \
-           'In0seyJjaWRycyI6WyI0Ni4wLjMxLjc1IiwiOTQuMjUwLjI1NS40NSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.QfmJEHMynExVx5_KCe1TnI' \
-           'gS_V87IJfI1XeXVStJTFMeZKzh0y1uCBlHm7vZeQwO6cnN6JS_NI9XlzJwkgeUrA'
+cr_token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFlMjRmY2ZlLThmZjctNGE0My04YmNlLWQ0MjU4YWU5YTk4MiIsImlhdCI6MTcxMTExOTY2Niwic3ViIjoiZGV2ZWxvcGVyL2YzNDllOGNjLTYzNTEtZDAxZC0wOGZhLWM0MzllMTgxZWI2NCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI1LjE2NC4xNDkuODYiXSwidHlwZSI6ImNsaWVudCJ9XX0.6cbC632JBTWxaKbiNmZfwHrldf0oppQCNpvBrjeTJMwNUPe7BplB5V5R8-h-ZDLg0nWpaBraW0d-HTsRTh43tw'
 
 cr_link = 'https://api.clashroyale.com/v1'
 
--- main.py
+++ main.py
@@ -22,75 +22,81 @@ def main():
             content = str(ct).split(" ")
             user_id = event.message.from_id
             peer_id = event.message.peer_id
-            if content[0][0] == '/':
-                user_profile = session_api.users.get(user_ids=user_id)[0]
-                user_first_name, user_last_name = user_profile['first_name'], user_profile['last_name']
-                print(f'{user_first_name} {user_last_name} написал(а): {ct}\n' + '=' * 30)
-                if content[0][1:] == 'help':
-                    session_api.messages.send(peer_id=peer_id, message=adds.f_help, random_id=0)
-                elif content[0][1:] == 'привет':
-                    session_api.messages.send(peer_id=peer_id, message='Привет!', random_id=0)
-                elif content[0][1:] == 'пока':
-                    session_api.messages.send(peer_id=peer_id, message='Пока!', random_id=0)
-                elif content[0][1:] == 'кубик':
-                    number = randint(1, 6)
-                    upload_result = upload_session.photo_messages(
-                        photos=str((utils.DICE_PATH / f'{number}.png').resolve())
-                    )
-                    picture = f"photo{upload_result[0]['owner_id']}_{upload_result[0]['id']}"
-                    session_api.messages.send(peer_id=peer_id, message=f'Вам выпало {number}!',
-                                              attachment=picture, random_id=0)
-                elif content[0][1:] == 'погода':
-                    geolocation = requests.get(
-                        'https://api.ipgeolocation.io/ipgeo',
-                        params=adds.g_api_params
-                    ).json()
-                    print(geolocation['latitude'], geolocation['longitude'])
-                    weather = requests.get(
-                        'https://api.weather.yandex.ru/v2/informers?',
-                        headers=adds.y_api_headers,
-                        params={'lat': 53.19171, 'lon': 50.19091, 'lang': 'ru_RU'}
-                    ).json()
-                    answer = f'Погода для Russia, Samara' \
-                             f'\nТемпература: {weather["fact"]["temp"]}°C' \
-                             f'\nОщущается как: {weather["fact"]["feels_like"]}°C' \
-                             f'\nСкорость ветра: {weather["fact"]["wind_speed"]} м/с'
-                    session_api.messages.send(peer_id=peer_id, message=answer, random_id=0)
-                elif content[0][1:] == 'сундуки':
-                    profile = requests.get(f'{adds.cr_link}/players/%23{content[1].upper()}',
-                                           headers=adds.cr_header).json()
-                    if len(profile["name"]) > 9:
-                        session_api.messages.send(peer_id=peer_id, message='Ты слабый', random_id=0)
-                        continue
-                    upcoming_chests = requests.get(f'{adds.cr_link}/players/%23{content[1].upper()}/upcomingchests',
-                                                   headers=adds.cr_header).json()
-                    result = Image.open('cr/back1.jpg', 'r')
-                    result.thumbnail((500, 500))
-                    index = 0
-                    for i in (10, 130, 260, 390):
-                        utils.add_chest(
-                            utils.get_chests()[upcoming_chests['items'][index]['name']],
-                            i, 150, result, index+1)
-                        index += 1
-                    for i in (10, 130, 260, 390):
-                        utils.add_chest(
-                            utils.get_chests()[upcoming_chests['items'][index]['name']],
-                            i, 300, result, index+1)
-                        index += 1
-                    utils.add_text(f'Следующие сундуки\n\nдля игрока',
-                                   result, (60, 30), (255, 255, 255))
-                    size_w, size_h = utils.add_text(profile['name'],
-                                                    result, (280, 84), (246, 100, 175))
-                    utils.add_text(':',
-                                   result, (280 + size_w, 81), (255, 255, 255))
-                    fp = BytesIO()
-                    result.save(fp, format='PNG')
-                    fp.seek(0)
-                    upload_result = upload_session.photo_messages(photos=fp)  # type: ignore
-                    picture = f"photo{upload_result[0]['owner_id']}_{upload_result[0]['id']}"
-                    session_api.messages.send(peer_id=peer_id, attachment=picture, random_id=0)
-            else:
-                print(f'{user_id} написал(а): {ct}\n' + '=' * 30)
+            user_profile = session_api.users.get(user_ids=user_id)[0]
+            user_first_name, user_last_name = user_profile['first_name'], user_profile['last_name']
+            print(f'{user_first_name} {user_last_name} написал(а): {ct}\n' + '=' * 30)
+            try:
+                if content[0][0] == '/':
+                    if content[0][1:] == 'help':
+                        session_api.messages.send(peer_id=peer_id, message=adds.f_help, random_id=0)
+                    elif content[0][1:] == 'привет':
+                        session_api.messages.send(peer_id=peer_id, message='Привет!', random_id=0)
+                    elif content[0][1:] == 'пока':
+                        session_api.messages.send(peer_id=peer_id, message='Пока!', random_id=0)
+                    elif content[0][1:] == 'кубик':
+                        number = randint(1, 6)
+                        upload_result = upload_session.photo_messages(
+                            photos=str((utils.DICE_PATH / f'{number}.png').resolve())
+                        )
+                        picture = f"photo{upload_result[0]['owner_id']}_{upload_result[0]['id']}"
+                        session_api.messages.send(peer_id=peer_id, message=f'Вам выпало {number}!',
+                                                  attachment=picture, random_id=0)
+                    elif content[0][1:] == 'погода':
+                        geolocation = requests.get(
+                            'https://api.ipgeolocation.io/ipgeo',
+                            params=adds.g_api_params
+                        ).json()
+                        print(geolocation['latitude'], geolocation['longitude'])
+                        weather = requests.get(
+                            'https://api.weather.yandex.ru/v2/informers?',
+                            headers=adds.y_api_headers,
+                            params={'lat': 53.19171, 'lon': 50.19091, 'lang': 'ru_RU'}
+                        ).json()
+                        answer = f'Погода для Russia, Samara' \
+                                 f'\nТемпература: {weather["fact"]["temp"]}°C' \
+                                 f'\nОщущается как: {weather["fact"]["feels_like"]}°C' \
+                                 f'\nСкорость ветра: {weather["fact"]["wind_speed"]} м/с'
+                        session_api.messages.send(peer_id=peer_id, message=answer, random_id=0)
+                    elif content[0][1:] == 'сундуки':
+                        profile = requests.get(f'{adds.cr_link}/players/%23{content[1].upper()}',
+                                               headers=adds.cr_header).json()
+                        if len(profile["name"]) > 9:
+                            session_api.messages.send(peer_id=peer_id, message='Ты слабый', random_id=0)
+                            continue
+                        upcoming_chests = requests.get(f'{adds.cr_link}/players/%23{content[1].upper()}/upcomingchests',
+                                                       headers=adds.cr_header).json()
+                        result = Image.open('cr/back1.jpg', 'r')
+                        result.thumbnail((500, 500))
+                        index = 0
+                        for i in (10, 130, 260, 390):
+                            chest = upcoming_chests['items'][index]['name']
+                            if chest == 'Tower Troop Chest':
+                                session_api.messages.send(peer_id=peer_id, message="эх, какие-то новые сундуки вас ждут, когда я был сделан, их ещё не существовало, староват я", random_id=0)
+                            else:
+                                utils.add_chest(
+                                utils.get_chests()[chest],
+                                i, 150, result, index+1)
+                            index += 1
+                        for i in (10, 130, 260, 390):
+                            utils.add_chest(
+                                utils.get_chests()[upcoming_chests['items'][index]['name']],
+                                i, 300, result, index+1)
+                            index += 1
+                        utils.add_text(f'Следующие сундуки\n\nдля игрока',
+                                       result, (60, 30), (255, 255, 255))
+                        size_w, size_h = utils.add_text(profile['name'],
+                                                        result, (280, 84), (246, 100, 175))
+                        utils.add_text(':',
+                                       result, (280 + size_w, 81), (255, 255, 255))
+                        fp = BytesIO()
+                        result.save(fp, format='PNG')
+                        fp.seek(0)
+                        upload_result = upload_session.photo_messages(photos=fp)  # type: ignore
+                        picture = f"photo{upload_result[0]['owner_id']}_{upload_result[0]['id']}"
+                        session_api.messages.send(peer_id=peer_id, attachment=picture, random_id=0)
+                else:
+                    session_api.messages.send(peer_id=peer_id, message="Список всех команд: /help", random_id=0)
+            except:
                 session_api.messages.send(peer_id=peer_id, message="Список всех команд: /help", random_id=0)
 
 
